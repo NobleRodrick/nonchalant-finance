a27696aee9040b51939980a7604b5b7b1eecc493
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2026-07-14 03:40:45 +0100

    the bottom page

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           {/* Footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with ❤️ by Nonchalant Alligator</p>
+              <p>Made with dedication, Springer Finance</p>
             </div>
           </footer>
         </body>
